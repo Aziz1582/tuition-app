f54442c0d9284839465515b269a9bae7e254a367
Aziz1582 <alonemandedu@gmail.com>
2025-07-18 00:03:56 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Find the <div id="root"> in public/index.html
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+// Render your App component
 root.render(<App />);
+
