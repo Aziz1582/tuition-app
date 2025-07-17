d53f734300218a59ce544948f4d8a9950c3e5dd3
Percy <alonemandedu@gmail.com>
2025-07-17 22:03:02 +0200

    Add login page and Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: '<YOUR-API-KEY>',
-  authDomain: '<YOUR-PROJECT>.firebaseapp.com',
-  projectId: '<YOUR-PROJECT-ID>',
-  storageBucket: '<YOUR-PROJECT>.appspot.com',
-  messagingSenderId: '<SENDER-ID>',
-  appId: '<APP-ID>'
+  apiKey: "AIzaSyD7wIbYOKUcHWIYM_yHY1ezyQWY6e-tFxY",
+  authDomain: "tuition-app-cda63.firebaseapp.com",
+  projectId: "tuition-app-cda63",
+  storageBucket: "tuition-app-cda63.firebasestorage.app",
+  messagingSenderId: "655725464291",
+  appId: "1:655725464291:web:b1274c20aa99e4f07813d6",
+  measurementId: "G-F2GWXPX48Q"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const analytics = getAnalytics(app);
